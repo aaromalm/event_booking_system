f02e0b340bc635adb64a901ebff4a0d55e683ab8
Aaromal M <aaromalm1032004@gmail.com>
2025-06-24 21:03:10 +0530

    fixed prblm with url in project

--- event_booking_project/urls.py
+++ event_booking_project/urls.py
@@ -17,15 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static 
-from django.http import HttpResponse
+from django.conf.urls.static import static
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('', include('event_booking_app.urls')),
-   path('', lambda request: HttpResponse("✅ EventHive is live!")),
+   path('', include('event_booking_app.urls')), 
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
